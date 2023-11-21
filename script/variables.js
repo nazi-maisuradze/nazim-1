f193d5a86b2490d3e1eab50c94c929e2a744d81b
nazi-maisuradze <146000732+nazi-maisuradze@users.noreply.github.com>
2023-11-21 21:58:31 +0400

    js

---
+++ script/variables.js
@@ -0,0 +1,13 @@
+
+
+let userInput =prompt("enter your firstname");
+alert ("Hello " + userInput);
+
+let userinput =prompt("enter your lastname");
+alert ("Hello " + userinput);
+
+let livingPlace = prompt("enter your living place");
+alert ("living place " + livingPlace)
+
+let yourAge = prompt("enter your age");
+alert ("Your age " + yourAge);
